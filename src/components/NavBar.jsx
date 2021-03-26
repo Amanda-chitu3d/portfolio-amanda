ab9bcb4bc7ff6c7d2a6fe2d90e68468eddc4c75e
Amanda <youhealth@outlook.com>
2021-03-26 18:05:13 +1100

    navbar color update, tag name

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,36 +3,36 @@ import {NavLink} from "react-router-dom";
 
 export default function NavBar() {
     return (
-        <header className="bg-green-900">
+        <header className="bg-blue-900">
             <div className="navbar container mx-auto flex justify-between">
                 <nav className="flex">
                 <NavLink to="/" exact 
                 activeClassName="text-white"
-                className="inline-flex items-center py-4 px-3 mr-6 text-green-100 hover:text-blue-300 text-4xl font-bold cursive tracking-widest"
+                className="inline-flex items-center py-4 px-3 mr-6 text-blue-100 hover:text-blue-300 text-4xl font-bold cursive tracking-widest"
                 >FalconWin</NavLink>
                 <NavLink to="/" exact 
-                className="inline-flex items-center py-3 px-3 mr-6 rounded text-green-200 hover:text-blue-300"
-                activeClassName="text-green-100 bg-green-700"
+                className="inline-flex items-center py-3 px-3 mr-6 rounded text-blue-200 hover:text-blue-300"
+                activeClassName="text-blue-100 bg-blue-700"
                 >Home</NavLink>
                 {/* <NavLink to="/post" 
-                className="inline-flex items-center py-3 px-3 mr-6 rounded text-green-200 hover:text-blue-300"
-                activeClassName="text-green-100 bg-green-700"
+                className="inline-flex items-center py-3 px-3 mr-6 rounded text-blue-200 hover:text-blue-300"
+                activeClassName="text-blue-100 bg-blue-700"
                 >Post</NavLink> */}
                 <NavLink to="/project" 
-                className="inline-flex items-center py-3 px-3 mr-6 rounded text-green-200 hover:text-blue-300"
-                activeClassName="text-green-100 bg-green-700"
+                className="inline-flex items-center py-3 px-3 mr-6 rounded text-blue-200 hover:text-blue-300"
+                activeClassName="text-blue-100 bg-blue-700"
                 >Projects</NavLink>
                 <NavLink to="/feature" 
-                className="inline-flex items-center py-3 px-3 mr-6 rounded text-green-200 hover:text-blue-300"
-                activeClassName="text-green-100 bg-green-700"
+                className="inline-flex items-center py-3 px-3 mr-6 rounded text-blue-200 hover:text-blue-300"
+                activeClassName="text-blue-100 bg-blue-700"
                 >Features</NavLink>
                 <NavLink to="/docs" 
-                className="inline-flex items-center py-3 px-3 mr-6 rounded text-green-200 hover:text-blue-300"
-                activeClassName="text-green-100 bg-green-700"
+                className="inline-flex items-center py-3 px-3 mr-6 rounded text-blue-200 hover:text-blue-300"
+                activeClassName="text-blue-100 bg-blue-700"
                 >Docs</NavLink>
                 <NavLink to="/about" 
-                className="inline-flex items-center py-3 px-3 mr-6 rounded text-green-200 hover:text-blue-300"
-                activeClassName="text-green-100 bg-green-700"
+                className="inline-flex items-center py-3 px-3 mr-6 rounded text-blue-200 hover:text-blue-300"
+                activeClassName="text-blue-100 bg-blue-700"
                 >About Me</NavLink>
 
                 </nav>
