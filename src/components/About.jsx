08fdbce2bf9361e6b89712d21fecd6deca6e23a9
Amanda <youhealth@outlook.com>
2021-03-27 09:15:03 +1100

    background color change

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -24,13 +24,13 @@ export default function About() {
         <main className="relative">
             <img src={background} alt="Avatar" className="absolute w-full"/>
             <div className="p-10 lg:pt-48 container mx-auto relative">
-                <section className="bg-green-700 rounded-lg shadow-2xl lg:flex p-24">
+                <section className="bg-blue-700 rounded-lg shadow-2xl lg:flex p-24">
                     <img src={urlFor(author.authorImage).url()} alt={author.name}
                     className="rounded w-32 h-32 lg:w-64 lg:h-64 mr-8"/>
                     <div className="text-lg flex flex-col justify-center">
-                        <h1 className="cursive text-6xl text-green-300 mb-4">
+                        <h1 className="cursive text-6xl text-blue-300 mb-4">
                             Hey there. I'm {""}
-                            <span className="text-green-200">{author.name}</span>
+                            <span className="text-blue-200">{author.name}</span>
                             </h1><div className="prose lg:prose-xl text-white">
                             <BlockContent blocks={author.bio} projectId="yu0v61qu"
                              dataset="production"/>
